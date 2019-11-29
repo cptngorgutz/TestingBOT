fef6efbcf3b7abf323b18d08a6109eaa6d31e36b
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-11-29 02:42:57 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -514,11 +514,11 @@ client.on('message', (message) => {
 const admin = [ '174307382296313857', '212232190358978560', '297448131698753538', '344950903910170655', '416730768388390912', '159953251691790336', '354421064815607808', ]
 if(admin.includes(message.author.id)){
 if(message.content.startsWith('!test')) {
+let MENTION = message.mentions.members.first();
 let modRole = message.guild.roles.find("name", "test");
 let creatorRole = message.guild.roles.find("name", "test2");
-if(message.mentions.member.roles.has(modRole.id) && message.mentions.member.roles.has(creatorRole.id)) {
+if(message.member.roles.has(modRole.id) && message.member.roles.has(creatorRole.id)) {
 let roleToAdd = message.guild.roles.find("name", "test3");
-let MENTION = message.mentions.members.first();
 MENTION.addRole(roleToAdd).catch(console.error);
 return message.reply("User had teams 1&2, now granted 3")
 } else {
