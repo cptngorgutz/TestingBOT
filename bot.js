f7794d348213eee15510e2522eef47a54826a92b
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-01 02:02:04 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -514,7 +514,7 @@ client.on('message', (message) => {
 const admin = [ '174307382296313857', '212232190358978560', '297448131698753538', '344950903910170655', '416730768388390912', '159953251691790336', '354421064815607808', ]
 if(admin.includes(message.author.id)){
 	//GIVE TEAM 1 REMOVE TEAM 2
-if(message.content.startsWith('!give TB2team1')) {
+if(message.content.startsWith('!give TB2team1') && message.content.contains(MENTION)) {
 const MENTION = message.mentions.members.first();
 const TB2 = message.guild.roles.find(role => role.name === 'TB2');
 const team1 = message.guild.roles.find(role => role.name === 'TB2team1');
@@ -524,17 +524,15 @@ MENTION.addRole(team1).catch(console.error);
 MENTION.removeRole(team2).catch(console.error);
 message.channel.send("Team2 Removed from " + MENTION + " + Team1 Added")
 } else {
-message.channel.send("Error")
 }
 //GIVE TEAM 1 REMOVE TEAM 3
-if(message.content.startsWith('!give TB2team1')) {
+if(message.content.startsWith('!give TB2team1') && message.content.contains(MENTION)) {
 const team3 = message.guild.roles.find(role => role.name === 'TB2team3');
 if(MENTION.roles.has(TB2.id) && MENTION.roles.has(team3.id)) {
 MENTION.addRole(team1).catch(console.error);
 MENTION.removeRole(team3).catch(console.error);
 message.channel.send("Team3 Removed from " + MENTION + " + Team1 Added")
 } else {
-message.channel.send("Error")
 }
 }}}});
 
