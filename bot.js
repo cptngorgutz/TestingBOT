f2e2ed04c56f18f8a9b35874f443d19b6d7857c8
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-03 02:25:34 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,44 +8,6 @@ const PREFIX2 = "p1-4!";
 //    console.log(`Logged in as ${client.user.tag}!`);
 //});
 
-// THIS IS FOR GIVEAWAYS
-const g_channel_id = '651246159356231680'; // This should be a String and the channel ID of the giveaways channel
-const g_message_id = '651246450134482944'; // This should be a String and the message ID of the givaway message
-const winner_amount = '1'; // This should be a Number and it represents how many winners you wish to choose
-       
-client.on('message', async message => {
-  if (message.author.bot) return;
-  
-  if (message.content.startsWith('randomuser')) {
-    let c = message.guild.channels.find(c => c.name === 'giveaways');
-    let m = await c.fetchMessage(MESSAGE_ID);
-    let reactions = m.reactions;
-    let reaction = reactions.first(); // This reaction would normally be the reaction a user would use to enter the giveawahy
-    let users = reaction.users.map(u => u.toString());
-    return message.channel.send(`**${users.random()}** has won the giveaway!`); // this uses the Array.prototpe.random method below
-  
-    const c = message.guild.channels.get(g_channel_id);
-    const m = await c.fetchMessage(g_message_id);
-    const reactions = m.reactions;
-    const reaction = reactions.first(); // This reaction would normally be the reaction a user would use to enter the giveaway
-    const users = reaction.users.map(u => u.toString());
-    if (winner_amount == 1) {
-    	return c.send(`**${users.random()}** has won the giveaway!`); // this uses the Array.prototpe.random method below
-    } else {
-    	let winners;
-    	const selected = randomAmount(users, winner_amount); // this uses the Array.prototype.randomAmount method below
-
-    	if (winner_amount === 2) winners = selected.join(' and ');
-    	else winners = selected.join(', ');
-
-    	return c.send(`**${winners}** have won the giveaway!`);
-    }
-  }      
-});
-
-//END OF GIVEAWAYS
-
-
 client.on('guildMemberAdd', function(member)
 {
 	let memberRole = member.guild.roles.find("name", "recruit");
