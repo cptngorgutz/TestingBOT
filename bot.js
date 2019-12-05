51ddf0e35a1b27f34b9ae4202d9da921feb6cc5d
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-05 12:55:41 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,13 +12,12 @@ client.on('guildMemberAdd', function(member)
 {
 	let memberRole = member.guild.roles.find("name", "recruit");
 	member.addRole(memberRole);
- 	member.guild.channels.get('651893277825564692').send(`Welcome to The Test Server! ${member}. \nPlease react below to join either The Beyonders 1, 2 or 3. \n(👍 For TB1) (👎 For TB2) (🖐️ For TB3) \nPlease post a profile picture in the #profile-images channel. Thankyou.`);
-	
+ 	member.guild.channels.get('551566689620197376').send(`Welcome to The Beyonders! ${member}. \nOne of the captains will be with you shortly. \nWe hope you enjoy your stay here. \nPlease post a profile picture in the #profile-images channel. Thankyou.`);
 	//NEW USERS JOIN
-	member.guild.channels.get('652123405990232066').send({embed: {
+	member.guild.channels.get('428968153800900608').send({embed: {
 	color: 0x00ff00, 
 	title: "**MEMBER JOINED! ** ",
-	description: member + " has joined **The Test ** discord server!",
+	description: member + " has joined **The Beyonders ** discord server!",
 	author: "Member Joined",
 	thumbnail: { url: member.user.displayAvatarURL },
 	fields: [{
@@ -31,23 +30,6 @@ client.on('guildMemberAdd', function(member)
 	}
 	}})
 });	 
-
-//** PRE-RECRUITMENT**
-client.on('messageReactionAdd', (messageReaction, user) => {
-if(user.bot)  return;
-const { message, emoji } = messageReaction;
-
-if(emoji.name === ":thumbsup:") {
-if(message.id === "652128693656879154") {
-let tb1recruitrole = member.guild.roles.find("name", "recruit");
-member.addRole(tb1recruitrole);
-message.guild.channels.find("name", "recruitment").sendMessage("" + member + " is interested in joining TB1");
-  }
- } 
-});
-
-
-//**pre-recruitment end**
 
 //********************** POLLS?***************
 //simple 2 responce poll yes or no
