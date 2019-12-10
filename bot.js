03e5ff18b9e89d1ad97e65596559ecdb3adf7913
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-10 17:25:14 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -902,20 +902,17 @@ message.channel.send("TB2 Removed from " + MENTION + " + TB1 Added, Random Team 
 if(message.content.startsWith('!give TB1 ') && message.content.includes(MENTION)) {
 const TB3 = message.guild.roles.find(role => role.name === 'TB3');
 if(MENTION.roles.has(TB3.id)) {
-const TB1 = message.guild.roles.find(role => role.name === 'TB1');
-const RANDOMTEAM = message.guild.roles.find(role => role.name === 'TB1team1');
-const MENTION = message.mentions.members.first();
 MENTION.addRole(TB1).catch(console.error);
 MENTION.removeRole(TB3).catch(console.error);
 MENTION.addRole(RANDOMTEAM).catch(console.error); 
 message.channel.send("TB3 Removed from " + MENTION + " + TB1 Added, Random Team Assigned.")
-}} else {
+} else {
 }}
 // GIVE TB1 WHILST HAVING TB1
 if(MENTION.roles.has(TB1.id)) {
-const MENTION = message.mentions.members.first();
 message.channel.send("" + MENTION + " Is already in TB1")
 } else {
+}}
 }});
 
 //*****************************************************************************************************//
