8dd23e12838d0882f3eab73c36a9d58b3d7f359e
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-10 16:50:13 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -899,6 +899,7 @@ message.channel.send("TB2 Removed from " + MENTION + " + TB1 Added, Random Team 
 }} else {
 }
 //GIVE TB1 REMOVE TB3
+const MENTION = message.mentions.members.first();
 if(message.content.startsWith('!give TB1 ') && message.content.includes(MENTION)) {
 const TB3 = message.guild.roles.find(role => role.name === 'TB3');
 if(MENTION.roles.has(TB3.id)) {
