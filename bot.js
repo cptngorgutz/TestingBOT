0f1cdd62b1c51e5035ffcf175f21c97be59a93d2
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-15 23:37:30 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1075,7 +1075,7 @@ client.on('guildMemberRemove', function(member)
 	message.delete(30000).catch(err => console.log(err));
 	};
 	if (message.content.includes('To claim a lane:'))  {												   
-	message.delete(5000).then(() => message.channel.send('!raid ultimus 6'));
+	message.delete(510000).then(() => message.channel.send('!raid ultimus 6'));
 	};
 	
 	if (message.attachments.size > 0) message.delete(5000);
