120b18adc7844240bb32f51e1a79e1a7e5c9337c
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-15 14:53:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1030,12 +1030,17 @@ client.on('guildMemberRemove', function(member)
 	message.channel.fetchMessages({limit: 45}).then(collected => { //collected is a Collection
 	collected.forEach(message => {
 	if (message.content.includes('Lane')) message.delete(60000);
-	if (message.content.includes('╔')) message.delete(25000);
+	if (message.content.includes('╔')) message.delete(27000);
 	if (message.content.includes('1')) message.delete(5000);
 	if (message.content.includes('2')) message.delete(10000);
 	if (message.content.includes('3')) message.delete(15000);	
-	if (message.content.includes('╚')) message.delete(20000);
-	if (message.content.includes('To claim a lane:')) message.delete(30000);
+	if (message.content.includes('4')) message.delete(18000);
+	if (message.content.includes('5')) message.delete(20000);
+	if (message.content.includes('6')) message.delete(22000);
+	if (message.content.includes('7')) message.delete(24000);
+	if (message.content.includes('8')) message.delete(25000);
+	if (message.content.includes('╚')) message.delete(30000);
+	if (message.content.includes('To claim a lane:')) message.delete(33000);
 	if (message.attachments.size > 0) message.delete(35000);
 	  })});
 }}});
