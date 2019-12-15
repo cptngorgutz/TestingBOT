3c6752f5424c6632ae9fbe4776e44ede34271c62
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-15 14:39:15 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1029,10 +1029,10 @@ client.on('guildMemberRemove', function(member)
 	if (message.channel.id === '655570619760377858' || message.channel.id === '617707484626288672' || message.channel.id === '655556582930776075') {
 	message.channel.fetchMessages({limit: 45}).then(collected => { //collected is a Collection
 	collected.forEach(message => {
-	if (message.content.includes('Lane')) message.delete(10);
+	if (message.content.includes('Lane')) message.delete(100);
 	if (message.content.includes('╔')) message.delete();
-	if (message.content.includes('1')) message.delete(5);
-	if (message.content.includes('2')) message.delete(100);
+	if (message.content.includes('1')) message.delete(1000);
+	if (message.content.includes('2')) message.delete(500);
 	if (message.content.includes('3')) message.delete();	
 	if (message.content.includes('╚')) message.delete();
 	if (message.content.includes('To claim a lane:')) message.delete();
