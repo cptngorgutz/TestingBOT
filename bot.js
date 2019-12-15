7d28f34f49b907cfef3b66748c73a7d32ca43447
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-15 13:48:40 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1021,22 +1021,35 @@ client.on('guildMemberRemove', function(member)
 	}
 	}}); });	
 	
-	client.on('message', function(message) {
-	if(message.author.bot) return;
-	if (message.content === '!refresh' || message.content === '!Refresh')	{
-//                           TB1 ULT 6 ROOM                             TB2 ULT 6 ROOM                              TB3 ULT 6 ROOM 
-	message.channel.bulkDelete(1)
-	var interval = setInterval (function () {
+client.on('message', function(message) {
+    // Now, you can use the message variable inside
 	if (message.channel.id === '655570619760377858' || message.channel.id === '617707484626288672' || message.channel.id === '655556582930776075') {
-	message.channel.fetchMessages({limit: 45}).then(collected => { //collected is a Collection
-	collected.forEach(message => {
-	if (message.content.includes('Lane') || message.content.includes('╔') || message.content.includes('1') || message.content.includes('2') || message.content.includes('3') || message.content.includes('╚') || message.content.includes('To claim a lane:')) message.delete();
-	if (message.attachments.size > 0) message.delete();
-	})})}
-	.catch(console.error);
-	 }, 1 * 1000); 
-	}
-});
+    if (message.content.includes('Lane') || message.content.includes('╔') || message.content.includes('1') || message.content.includes('2') || message.content.includes('3') || message.content.includes('╚') || message.content.includes('To claim a lane:')) {
+        var interval = setInterval (function () {
+            // use the message's channel (TextChannel) to send a new message
+             message.delete();
+            .catch(console.error); // 
+        }, 1 * 100); 
+    }
+}
+});	
+	
+//	client.on('message', function(message) {
+//	if(message.author.bot) return;
+//	if (message.content === '!refresh' || message.content === '!Refresh')	{
+//                           TB1 ULT 6 ROOM                             TB2 ULT 6 ROOM                              TB3 ULT 6 ROOM 
+//	message.channel.bulkDelete(1)
+//	var interval = setInterval (function () {
+//	if (message.channel.id === '655570619760377858' || message.channel.id === '617707484626288672' || message.channel.id === '655556582930776075') {
+//	message.channel.fetchMessages({limit: 45}).then(collected => { //collected is a Collection
+//	collected.forEach(message => {
+//	if (message.content.includes('Lane') || message.content.includes('╔') || message.content.includes('1') || message.content.includes('2') || message.content.includes('3') || message.content.includes('╚') || message.content.includes('To claim a lane:')) message.delete();
+//	if (message.attachments.size > 0) message.delete();
+//	})})}
+//	.catch(console.error);
+//	 }, 1 * 1000); 
+//	}
+//});
 
 	
 
