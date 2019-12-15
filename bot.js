22f74c84234409704aeab132233d5b749d08f5dd
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-15 18:03:25 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1030,34 +1030,34 @@ client.on('guildMemberRemove', function(member)
 	message.channel.fetchMessages({limit: 45}).then(collected => { //collected is a Collection
 	collected.forEach(message => {
 	if (message.content.includes('╔'))  {
-	message.delete(277000).catch(err => console.log(err));
+	message.delete(480000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 1 '))  {
-	message.delete(240000).catch(err => console.log(err));
+	message.delete(420000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 2 '))  {
-	message.delete(225000).catch(err => console.log(err));
+	message.delete(360000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 3 '))  {
-	message.delete(195000).catch(err => console.log(err));
+	message.delete(320000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 4 '))  {
-	message.delete(165000).catch(err => console.log(err));
+	message.delete(240000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 5 '))  {
-	message.delete(130000).catch(err => console.log(err));
+	message.delete(180000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 6 '))  {
-	message.delete(95000).catch(err => console.log(err));
+	message.delete(120000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 7 '))  {
-	message.delete(65000).catch(err => console.log(err));
+	message.delete(60000).catch(err => console.log(err));
 	};
 	if (message.content.includes('Lane 8 '))  {
-	message.delete(35000).catch(err => console.log(err));
+	message.delete(30000).catch(err => console.log(err));
 	};
 	if (message.content.includes('To claim a lane:'))  {
-	message.delete(305000).catch(err => console.log(err));
+	message.delete(540000).catch(err => console.log(err));
 	};
 	if (message.attachments.size > 0) message.delete(5000);
 	  })});
