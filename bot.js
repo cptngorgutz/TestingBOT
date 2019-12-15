49402543d89e55fc862346046a2613e0b400332e
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-15 21:51:29 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,7 +84,7 @@ client.on('message', async message =>{
 			let msgArgs = args.slice(1).join(" ");
 			
 			message.channel.send(msgArgs).then(async messageReaction => {
-				await messageReaction.react("651124121005981722");
+				await messageReaction.react("651124121005981722"); 
 				await messageReaction.react("651124121119359006");
 				await messageReaction.react("651124121371017266");
 				await messageReaction.react("651124120913838121");
@@ -1027,9 +1027,16 @@ client.on('guildMemberRemove', function(member)
 //                           TB1 ULT 6 ROOM                             TB2 ULT 6 ROOM                              TB3 ULT 6 ROOM 
 	message.channel.bulkDelete(1)
 	if (message.channel.id === '655570619760377858' || message.channel.id === '617707484626288672' || message.channel.id === '655556582930776075') {
-	message.channel.send(550000)("!raid ultimus 6")
+	message.channel.send("!raid ultimus 6")
 	}}});	
 
+	
+	
+	
+	
+	
+	
+	
 	
 
 	client.on('message', (message) => { //hereboy2
@@ -1068,8 +1075,9 @@ client.on('guildMemberRemove', function(member)
 	message.delete(30000).catch(err => console.log(err));
 	};
 	if (message.content.includes('To claim a lane:'))  {
-	message.delete(540000).catch(err => console.log(err)).then(message.channel.send(550000)("!raid ultimus 6"))
-	};
+	message.delete(540000).catch(err => console.log(err)).then(async messageRAID => {
+	await messageRAID(54000).then(message.channel.send("!raid ultimus 6"))
+	}});
 	if (message.attachments.size > 0) message.delete(5000);
 	  })});
 }}});
