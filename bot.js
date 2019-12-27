8eabc401364f75aad72bdce57bbed69a2fa685b5
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-27 13:55:17 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,16 +40,18 @@ client.on('guildMemberAdd', member => {
     const logChannel = member.guild.channels.find(c => c.name === 'actual-logs');
     // A real basic message with the information we need. 
 //    logChannel.send(`${member.user.tag} joined using  code ${invite.code} from ${inviter.tag}. Invite was used ${invite.uses} times since its creation.`);
-	let memberRole = member.guild.roles.find(r => r.name === "recruit");
 if (invite.code === "wgcNhDk") {
+let memberRole = member.guild.roles.find(r => r.name === "recruit");
 member.addRole(recruit);
 logChannel.send("" + member + "is looking to join TB1");
 }
 if (invite.code === "bC4dBS3") {
+let memberRole = member.guild.roles.find(r => r.name === "recruit");
 logChannel.send("" + member + "is looking to join TB2");
 member.addRole(recruit);
 }
 if (invite.code === "qAmtPAZ") {
+let memberRole = member.guild.roles.find(r => r.name === "recruit");
 logChannel.send("" + member + "is looking to join TB3");
 member.addRole(recruit);
 }
