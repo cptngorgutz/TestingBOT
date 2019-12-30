d41ef52dcbe5f52d850ff18241024378491dd036
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-30 02:39:43 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,10 +92,10 @@ client.on('message', message => {
       channelToCheck.fetchMessages({ limit: 1 }).then(messages => {
         const lastMessage = messages.first()
 		var blitzimage = (message.attachments)
-        message.channel.send(lastMessage.content)
 		if (message.attachments.size > 0) {
 		message.channel.send(blitzimage);}
-		message.channel.send(lastMessage)
+		else {
+		message.channel.send(lastMessage.content)
 		}
       }).catch(err => {
         console.error(err)
