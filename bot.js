d019f78d310bd878db0620ad9b9b6d6a9cfde26c
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-30 13:13:39 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,12 +90,11 @@ client.on('message', message => {
 
       // Fetch the last message from the mentioned channel.
       channelToCheck.fetchMessages({ limit: 1 }).then(messages => {
- //       const lastMessage = messages.first()
-//		message.channel.send(lastMessage.content)
-			var imagefile = (message.attachments)
-			if (imagefile){
-			message.channel.send(imagefile)
-  }
+		  
+	if (message.attachments.size !== 0) { // Attachments are present.
+    const firstAttachment = message.attachments.first();
+    console.log(`haha ${firstAttachment.url}`);
+	
       }).catch(err => {
         console.error(err)
       })
