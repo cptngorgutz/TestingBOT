a6a606c2aa1a4cc2e9c87f169565a106b0a32a52
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-30 12:55:47 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ client.on('message', message => {
       // Fetch the last message from the mentioned channel.
 		channelToCheck.fetchMessages({ limit: 1 }).then(messages => {
 		const lastImage = message.attachments.first()
-		message.channel.send(lastImage)
+		message.channel.send(lastImage.attachment)
 	})
     }
 	 }
