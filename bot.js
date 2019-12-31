cc8815f5f2f1e86e0b7516f8ac63b87feef537a3
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2019-12-31 01:20:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -114,11 +114,11 @@ if (msg.content === '!Asgardians') {
 	msg.channel.bulkDelete(1)
 	const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
-	..addField(("<:heimdall:661370019325935666>" + "** Heimdall** \nHeimdall's Speed is: 100", '')
-	..addField(("<:hela:661370019317284880>" + "** Hela** \nHela's Speed is: 110", '⠀')
-	..addField("<:sif:661370019300638737>" + "** Sif** \nSif's Speed is: 108", '⠀')
-	..addField(("<:loki:661370019145449473>" + "** Loki** \nLoki's Speed is: 115", '⠀')
-	..addField(("<:thor:661370019279667248>" + "** Thor** \nThor's Speed is: 90", '⠀')
+	.addField(("<:heimdall:661370019325935666>" + "** Heimdall** \nHeimdall's Speed is: 100", '')
+	.addField(("<:hela:661370019317284880>" + "** Hela** \nHela's Speed is: 110", '⠀')
+	.addField("<:sif:661370019300638737>" + "** Sif** \nSif's Speed is: 108", '⠀')
+	.addField(("<:loki:661370019145449473>" + "** Loki** \nLoki's Speed is: 115", '⠀')
+	.addField(("<:thor:661370019279667248>" + "** Thor** \nThor's Speed is: 90", '⠀')
 	msg.channel.send(exampleEmbed);
 }});
 
