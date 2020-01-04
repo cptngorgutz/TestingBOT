5b02162176d8667c5bbc34873ee5d9a262b960d6
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-04 00:19:13 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -334,17 +334,16 @@ if (msg.content === '!yo-yo T4') {
 	msg.channel.send(exampleEmbed4);
 }});
 
-//unlock shuri
 client.on('message', msg => {
-if (msg.content === '!unlock shuri') {
-	msg.channel.bulkDelete(1)
-	const exampleEmbed = new Discord.RichEmbed()
-	.setColor('#0099ff')
-	.setDescription("__**Shuri Unlock**__ \n<:staron:662797309205151744><:staron:662797309205151744><:staron:662797309205151744><:staron:662797309205151744><:staron:662797309205151744><:staroff:662797308835921935><:staroff:662797308835921935> \n \n**Spiderverse** \n‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎ ‎‎ ‎‎<:heimdall:661370019325935666>‏‏‎‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎ ‎‎ ‎‎<:heimdall:661370019325935666>‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎ ‎‎ ‎‎<:heimdall:661370019325935666>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‎ ‎‎‎<:heimdall:661370019325935666>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ ‎‎ ‎‎<:heimdall:661370019325935666>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ ‎‎ ‎‎<:heimdall:661370019325935666>‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ \nPERSON1 PERSON1 PERSON1 PERSON1 PERSON1 PERSON1 \n \n**Sinister six** \n‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎ ‎‎ ‎‎<:sif:661370019300638737>‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎  ‎‎ ‎‎‎<:sif:661370019300638737>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎ ‎‎ ‎‎<:sif:661370019300638737>‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ ‎‎ ‎‎<:sif:661370019300638737>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‎ ‎‎ ‎‎<:sif:661370019300638737>‏‏‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎ ‎‎ ‎‎<:sif:661370019300638737>‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎‏‏‎ ‎\nPERSON1 PERSON1 PERSON1 PERSON1 PERSON1 PERSON1")
-	msg.channel.send(exampleEmbed);
-}});
-
-
+if (msg.content === '!status') {
+if (member.presence.clientStatus = 'desktop') {
+console.log("using computer")
+}
+if (member.presence.clientStatus = 'mobile') {
+console.log("using mobile")
+}
+}
+});
 
 
 
