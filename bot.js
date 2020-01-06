236b1c6e1fa9ddc723a947e4203316cb697a36f0
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-06 19:18:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -347,11 +347,8 @@ client.on('message', message => {
       // Fetch the last message from the mentioned channel.
 	if (message.attachments.size !== 0) { // Attachments are present.
     const firstAttachment = message.attachments.first();
-    message.channel.send(firstAttachment.url);
+    message.channel.send({firstAttachment.url});
 	}
-    .catch(err => {
-    console.error(err)
-    })
 }
 }
 });
