3adaf314286346779ee1a5090c46cd5d8942fa0d
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-06 16:30:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -347,7 +347,7 @@ client.on('message', message => {
 
       // Fetch the last message from the mentioned channel.
     channelToCheck.fetchMessages({ limit: 1 }).then(messages => {
-    const lastMessage = messages.first()
+    const lastMessage = messages.attachment.url()
 	message.channel.send(lastMessage.content)
     }).catch(err => {
     console.error(err)
@@ -355,6 +355,11 @@ client.on('message', message => {
     }
   }
 });
+
+
+
+
+
 
 
 
