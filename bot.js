f31a4eeea0820d8364e838500fb88e84a2c718c5
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-06 23:43:19 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -347,7 +347,7 @@ client.on('message', message => {
 
     channelToCheck.fetchMessages({ limit: 1 }).then(messages => {
     const lastMessage = messages.first().attachments.first()
-	message.channel.send(lastMessage)
+	message.channel.send(lastMessage.attachment)
     }).catch(err => {
     console.error(err)
     })
