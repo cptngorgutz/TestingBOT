0715d9b7752061089acebdaa9431706d8613f4a7
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-12 21:36:30 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -969,7 +969,7 @@ const exampleEmbed1 = new Discord.RichEmbed()
 .setColor('#0099ff')
 .setDescription(LastText.content)
  // return message.channel.send(exampleEmbed1);
-  return embed.send(exampleEmbed1);
+  return message.channel.send({ embed: exampleEmbed1 })
 })
 .then( embed1 => channelToCheckImagesallin1.fetchMessages({limit: 1}))
 .then( messages => {
@@ -980,7 +980,7 @@ const exampleEmbed2 = new Discord.RichEmbed()
 .setColor('#0099ff')
 .setImage(lastMessage.url) 
 //return message.channel.send(exampleEmbed2);
-return embed.send(exampleEmbed2);
+  return message.channel.send({ embed: exampleEmbed2 })
 })
 .then( embed2 => channelToCheckYT.fetchMessages({limit: 1}))
 .then( messages => {
@@ -989,7 +989,7 @@ const exampleEmbed3 = new Discord.RichEmbed()
 .setColor('#0099ff')
 .setDescription(LastYT.content)
 //return message.channel.send(exampleEmbed3);
- return embed.send(exampleEmbed3);
+  return message.channel.send({ embed: exampleEmbed3 })
 });
 }
 });
