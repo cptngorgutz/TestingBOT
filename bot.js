a97786214a90e450ae85448c5943e458d0409322
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-12 01:15:51 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -872,7 +872,7 @@ client.on('message', message => {
     channelToCheckImages.fetchMessages({ limit: 6 }).then(messages => {
 	const Attachment = require('discord.js').Attachment;
 	const attachment = new Attachment(Attachment.url)
-	messages.first().attachments.forEach(attachment => {
+	messages.first().attachments.forEach(Attachment => {
 	message.channel.send(attachment);
 	});
     }).catch(err => {
