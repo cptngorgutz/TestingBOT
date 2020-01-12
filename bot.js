a5e4a895438bd4e101675d1e71eb219177460424
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-12 00:12:39 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -873,7 +873,7 @@ client.on('message', message => {
 	const lastMessage = messages.first().attachments.first()
 	const Attachment = require('discord.js').Attachment;
 	const attachment = new Attachment(lastMessage.url)
-	attachment.forEach(function(attachment) {
+	attachment(function(attachment) {
 	message.channel.send(attachment);
 	})
     })
