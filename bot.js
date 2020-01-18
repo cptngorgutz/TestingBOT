8d39f8f49cf03ae14dcefd08473f8d2895f854ca
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-18 01:39:51 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1929,8 +1929,9 @@ CheckText.send("War Counter Submission Received.");
 client.on("message", message => {
   if (message.author.bot) return;
   // This is where we'll put our code.
-  if (message.content.indexOf(config.prefix) !== 0) return;
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const PREFIX = "!";
+  if (message.content.indexOf(PREFIX) !== 0) return;
+  const args = message.content.slice(PREFIX.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
 	if(command === 'daredevil unlock') {
