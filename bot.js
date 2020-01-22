f74f1d3a4b22d450f0af40a7cd4d6abf757bdca5
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-01-22 20:06:46 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -2129,7 +2129,7 @@ message.channel.send("asgardian and avenger")
 
 //AWAY NOTES
 client.on("message", async message => {
-if(message.content.startswith('!away')) {
+if(message.content.startsWith('!away')) {
 const awaynoteschannel = client.channels.get('666305824813219870')
 const messages1 = await message.channel.fetchMessages({ limit: 1 });
 const embed = new Discord.RichEmbed()
