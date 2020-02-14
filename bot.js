87c6c0a6d91cdd68c706673ced75f075817c4a4d
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-02-14 14:15:50 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3483,7 +3483,7 @@ if (message.content === '!comm') {
 });
 
 
-
+//RAIDS AND WAR COMMANDS
 client.on("message", message => {
 const args = message.content.toLowerCase().slice(config.prefix.length).trim().split(/ +/g);
 const command = args.shift().toLowerCase();
@@ -3494,12 +3494,12 @@ let ultimus = args[0];
 
 
 //ULTIMUS & EVENT RAIDS
-if (message.channel.id === '666305824813219870' || message.channel.id === '617707484626288672' || message.channel.id === '661221254958940220') { //bot spam channel
-if(command === 'raid') {
 const TB1captains = message.guild.roles.find(role => role.name === 'TB1 Captain');
 const TB2captains = message.guild.roles.find(role => role.name === 'TB2 Captain');
 const TB3captains = message.guild.roles.find(role => role.name === 'TB3 Captain');
 if(message.member.roles.has(TB1captains.id) || message.member.roles.has(TB2captains.id) || message.member.roles.has(TB3captains.id)) {
+if (message.channel.id === '666305824813219870' || message.channel.id === '617707484626288672' || message.channel.id === '661221254958940220') { //BOT SPAM CHANNEL
+if(command === 'raid') {
 if(alpha === "alpha" && args[1] === "4"){
 	const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
@@ -3530,10 +3530,210 @@ if(ultimus === "ultimus" && args[1] === "7"){
 	.setDescription("**Ultimus 7 would have launched**.")
 	message.channel.send(exampleEmbed);
 }
+}//END OF RAID COMMAND
+}//IF USER IS IN BOT CHANNELS
+if(command === 'u6') {
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Ultimus 6 Launched")  
+}
+if(command === 'u7') {
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Ultimus 7 Launched")  
+}
+if(command === 'a4') {
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Alpha 4 Launched")  
+}
+if(command === 'b4') {
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Beta 4 Launched")  
+}
+if(command === 'g4') {
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Gamma 4 Launched")  
+}
+
+//WAR COMMANDS
+if (message.channel.id === '666305824813219870' || message.channel.id === '617707484626288672' || message.channel.id === '661221254958940220') { //WAR CHANNEL
+let l = args[0];
+let r = args[0];
+let f = args[0];
+let ffa = args[0]; 
+if(command === 'fd1') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 1 - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 1 - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 1 - Full Clear")  
 }
 }
+if(command === 'fd2') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 2 - Left Side")  
 }
-});
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 2 - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 2 - Full Clear")  
+}
+}
+if(command === 'fd3') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 3 - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 3 - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Flight Deck 3 - Full Clear")  
+}
+}
+if(command === 'han') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Hangar - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Hangar - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Hangar - Full Clear")  
+}
+}
+if(command === 'eng') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Engineering - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Engineering - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Engineering - Full Clear")  
+}
+}
+if(command === 'bar') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Barracks - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Barracks - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Barracks - Full Clear")  
+}
+}
+if(command === 'rea') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Reactor - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Reactor - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Reactor - Full Clear")  
+}
+}
+if(command === 'arm') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Armory - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Armory - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Armory - Full Clear")  
+}
+}
+if(command === 'bri') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Bridge - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Bridge - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Bridge - Full Clear")  
+}
+}
+if(command === 'car') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Cargo - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Cargo - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Cargo - Full Clear")  
+}
+}
+if(command === 'sec') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Security - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Security - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Security - Full Clear")  
+}
+}
+if(command === 'med') {
+if(l === "l"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Medbay - Left Side")  
+}
+if(r === "r"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Medbay - Right Side")  
+}
+if(f === "f"){
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Medbay - Full Clear")  
+}
+}
+if(command === 'ffa') {
+message.channel.bulkDelete(1)
+message.channel.send("@everyone Free For All")  
+}
+}//IF USER IS IN WAR CHANNELS
+}//IF USER HAS ROLES TB1/2/3 CAPTAIN
+}); //END OF CLIENT
 
 
 
