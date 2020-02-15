3595761a04aaac41f5cd21b67ad4328c07bcf0d7
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-02-15 00:46:45 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3497,7 +3497,6 @@ const TB1captains = message.guild.roles.find(role => role.name === 'TB1 Captain'
 const TB2captains = message.guild.roles.find(role => role.name === 'TB2 Captain');
 const TB3captains = message.guild.roles.find(role => role.name === 'TB3 Captain');
 if(message.member.roles.has(TB1captains.id) || message.member.roles.has(TB2captains.id) || message.member.roles.has(TB3captains.id)) {
-console.error(err);
 if (message.channel.id === '666305824813219870' || message.channel.id === '617707484626288672' || message.channel.id === '661221254958940220') { //BOT SPAM CHANNEL
 if(command === 'raid') {
 if(alpha === "alpha" && args[1] === "4"){
@@ -4770,8 +4769,6 @@ message.channel.send("@everyone Free For All")
 
 
 //IF USER HAS ROLES TB1/2/3 CAPTAIN
-} else {
-	console.log(`User doesn't have role for purging.`);
 }
 }); //END OF CLIENT
 
