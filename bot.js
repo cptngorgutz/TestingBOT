558f7f32c42044aaef2f5c0afe2323337ed86897
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-11 13:23:33 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11443,7 +11443,7 @@ const rolelistactual = [ '431511357540532244','431511377824448512','431511398057
 const rolelist = [ '653653557475803137','653653611741446144','653653648655777830','649763104057720853','649763122688688129','649763083052384260','653653699285221419','653653722739769344','653653761012531208',]
 //                    GADD                   BOB                   CLEAVELANDS          RAIN                 VAYGRANT               BUSTA
 const cap = [ '678031406483374101', '678038936496635907', '678031440700506132', ]
-if(cap.includes(message.member.has.role(cap.id))){
+if(message.member.roles.has(cap.id)){
 if(command === 'give' && args[0] === "tb1") { 
 //GIVING TB1
 if(mentionedUser.roles.has(TB2.id)) { //GIVE TB1 REMOVE TB2
