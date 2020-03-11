6e2f31979277b8af610c4b442b2ecacb3e050953
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-11 13:35:58 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -11438,12 +11438,15 @@ const recruit = message.guild.roles.find(role => role.name === 'recruit');
 const TB1 = message.guild.roles.find(role => role.name === 'TB1');
 const TB2 = message.guild.roles.find(role => role.name === 'TB2');
 const TB3 = message.guild.roles.find(role => role.name === 'TB3');
+const TB1cap = message.guild.roles.find(role => role.name === 'TB1 Captain');
+const TB2cap = message.guild.roles.find(role => role.name === 'TB2 Captain');
+const TB3cap = message.guild.roles.find(role => role.name === 'TB3 Captain');
 //                       TB1team1             TB1team2            TB1team3              TB2team1             TB2team2            TB2team3             TB3team1              TB3team2             TB3team3
 const rolelistactual = [ '431511357540532244','431511377824448512','431511398057771029','486675509166735371','486675593522446346','486675606021341204','643129349926682635','643129351549878295','643129353873391657',]
 const rolelist = [ '653653557475803137','653653611741446144','653653648655777830','649763104057720853','649763122688688129','649763083052384260','653653699285221419','653653722739769344','653653761012531208',]
 //                    GADD                   BOB                   CLEAVELANDS          RAIN                 VAYGRANT               BUSTA
 const cap = [ '678031406483374101', '678038936496635907', '678031440700506132', ]
-if(message.member.roles.has(cap.id)){
+if(message.member.roles.has(TB1cap.id) || message.member.roles.has(TB2cap.id) || message.member.roles.has(TB3cap.id) {
 if(command === 'give' && args[0] === "tb1") { 
 //GIVING TB1
 if(mentionedUser.roles.has(TB2.id)) { //GIVE TB1 REMOVE TB2
