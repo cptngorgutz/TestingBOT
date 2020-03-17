bcb88aa0c0aedc14abef20fd877cf26a541f0c15
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-17 17:04:20 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8478,7 +8478,6 @@ fs.readFile('credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
   // Authorize a client with credentials, then call the Google Sheets API.
   authorize(JSON.parse(content), listMajors);
-  authorize(JSON.parse(content), listers);
 });
 
 /**
