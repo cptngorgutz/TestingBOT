c8a3b4ec54ed51e3624cfd045dd33cba36de3a75
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-21 05:17:07 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8635,14 +8635,12 @@ const args = message.content.toLowerCase().slice(config.prefix.length).trim().sp
 const command = args.shift().toLowerCase();
 const { google } = require("googleapis");
 const auth = require("./credentials-load");
-  const channelToCheck = message.guild.channels.get('617707484626288672')
-  channelToCheck.fetchMessages({ limit: 1 }).then(messages => {
-  const lastMessage = messages.first()
-  })
+
 async function run() {
   //create sheets client
   const sheets = google.sheets({ version: "v4", auth });
   if (command === 'update'){
+const lastMessage = messages.first()
   const res = await sheets.spreadsheets.values.update({
     spreadsheetId: "1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw",
     range: "Sheet1!A13",
