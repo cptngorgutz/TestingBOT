ecd9e8b542dfcab2d533edc3600008fae8716f60
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-23 03:05:54 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8774,7 +8774,7 @@ sheets.spreadsheets.values.get(
 );
 	const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
-	.setDescription("Added: " + args[1] + asgardnumber + " vs " + args[2] + xmennumber )
+	.setDescription("Added: " + args[0] + args[1] + " vs " + args[2] + args[3])
 	message.channel.send(exampleEmbed); 
 }
 
