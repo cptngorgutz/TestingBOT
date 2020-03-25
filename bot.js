a5adbf207922bc2c9876d9ca0c03619d54a261da
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-25 14:27:11 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -8995,7 +8995,23 @@ if (command === 'counter' && args[0] === "aim+"){
     }
   });
 }
-
+if (command === 'asgardians' && args[0] === "stats"){
+	  sheets.spreadsheets.values.get({
+    spreadsheetId: '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw',
+    range: "ControlPanel!A1:C",
+  }, (err, res) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res.data.values;
+    if (rows.length) {
+	const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setDescription(rows.join('\n'))
+	message.channel.send(exampleEmbed); 
+    } else {
+      console.log('No data found.');
+    }
+  });
+}
 
 
 if (command === 'geta'){
