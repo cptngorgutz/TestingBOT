4fbe122ac82faee7317c57cb9511985b84ab26c5
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-25 15:26:10 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9015,14 +9015,14 @@ if (command === 'asgardians' && args[0] === "stats"){
 if (command === 'aim' && args[0] === "stats"){
 	  sheets.spreadsheets.values.get({
     spreadsheetId: '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw',
-    range: "ControlPanel!D1:F"(' '),
+    range: "ControlPanel!D1:F",
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
     if (rows.length) {
 	const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
-	.setDescription(rows.join('\n'))
+	.setDescription(rows.join('\n').replace(','', '''))
 	message.channel.send(exampleEmbed); 
     } else {
       console.log('No data found.');
