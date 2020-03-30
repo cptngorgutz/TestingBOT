c097556ff9eab617161a61df26f7141192493c8f
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-03-30 16:32:13 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -57769,7 +57769,7 @@ sheets.spreadsheets.values.get(
 );
 }
 message.channel.send("‎");
-message.delete(30000).catch();
+message.delete(5000).catch();
 if (args[2] === "han" || args[2] === "car" || args[2] === "rea" || args[2] === "sec" || args[2] === "eng" || args[2] === "med" || args[2] === "bar" || args[2] === "arm" || args[2] === "bri"){
 const mySpreadSheetId = '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw';
 const sheetName = "WarMatchups";
@@ -57811,7 +57811,7 @@ sheets.spreadsheets.values.get(
 
 }
 message.channel.send("‎");
-message.delete(60000).catch();
+message.delete(8000).catch();
 if (args[3] === "han" || args[3] === "car" || args[3] === "rea" || args[3] === "sec" || args[3] === "eng" || args[3] === "med" || args[3] === "bar" || args[3] === "arm" || args[3] === "bri"){
 const mySpreadSheetId = '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw';
 const sheetName = "WarMatchups";
@@ -57853,7 +57853,7 @@ sheets.spreadsheets.values.get(
 
 }
 message.channel.send("‎");
-message.delete(90000).catch();
+message.delete(10000).catch();
 if (args[4] === "han" || args[4] === "car" || args[4] === "rea" || args[4] === "sec" || args[4] === "eng" || args[4] === "med" || args[4] === "bar" || args[4] === "arm" || args[4] === "bri"){
 const mySpreadSheetId = '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw';
 const sheetName = "WarMatchups";
@@ -57895,7 +57895,7 @@ sheets.spreadsheets.values.get(
 
 }
 message.channel.send("‎");
-message.delete(120000).catch();
+message.delete(12000).catch();
 if (args[5] === "han" || args[5] === "car" || args[5] === "rea" || args[5] === "sec" || args[5] === "eng" || args[5] === "med" || args[5] === "bar" || args[5] === "arm" || args[5] === "bri"){
 const mySpreadSheetId = '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw';
 const sheetName = "WarMatchups";
@@ -57937,7 +57937,7 @@ sheets.spreadsheets.values.get(
 
 }
 message.channel.send("‎");
-message.delete(150000).catch();
+message.delete(15000).catch();
 if (args[6] === "han" || args[6] === "car" || args[6] === "rea" || args[6] === "sec" || args[6] === "eng" || args[6] === "med" || args[6] === "bar" || args[6] === "arm" || args[6] === "bri"){
 const mySpreadSheetId = '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw';
 const sheetName = "WarMatchups";
@@ -57979,7 +57979,7 @@ sheets.spreadsheets.values.get(
 
 }
 message.channel.send("‎");
-message.delete(180000).catch();
+message.delete(18000).catch();
 if (args[7] === "han" || args[7] === "car" || args[7] === "rea" || args[7] === "sec" || args[7] === "eng" || args[7] === "med" || args[7] === "bar" || args[7] === "arm" || args[7] === "bri"){
 const mySpreadSheetId = '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw';
 const sheetName = "WarMatchups";
