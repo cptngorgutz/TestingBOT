86dc3a303345e767238d80804267e37cbfd3b6fb
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-04-27 14:45:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,30 @@
 const Discord = require('discord.js');
+const Canvas = require('canvas');
+
 const client = new Discord.Client();
 
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
 client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-	channel.send(`Welcome to the server, ${member}!`);
+	const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+	if (!channel) return;
+
+	const canvas = Canvas.createCanvas(700, 250);
+	const ctx = canvas.getContext('2d');
+
+	// Since the image takes time to load, you should await it
+	const background = await Canvas.loadImage('./wallpaper.jpg');
+	// This uses the canvas dimensions to stretch the image onto the entire canvas
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+	// Use helpful Attachment class structure to process the file for you
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'welcome-image.png');
+
+	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
 
-client.on('message', message => {
-	if (message.content === '!join') {
-		client.emit('guildMemberAdd', message.member);
-	}
-});
 
 
 client.login(process.env.TOKEN);
