8e3fd91ca9866a65d5e644520f099b3c63f5ee7f
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-04-27 17:08:59 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,36 +13,5 @@ client.on('message', message => {
 	}
 });
 
-client.on('guildMemberAdd', async member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'member-log');
-	if (!channel) return;
-
-	const { createCanvas, loadImage } = require('canvas')
-const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d')
-
-// Write "Awesome!"
-ctx.font = '30px Impact'
-ctx.rotate(0.1)
-ctx.fillText('Awesome!', 50, 100)
-
-// Draw line under text
-var text = ctx.measureText('Awesome!')
-ctx.strokeStyle = 'rgba(0,0,0,0.5)'
-ctx.beginPath()
-ctx.lineTo(50, 102)
-ctx.lineTo(50 + text.width, 102)
-ctx.stroke()
-
-// Draw cat with lime helmet
-loadImage('./wallpaper.jpg').then((image) => {
-  ctx.drawImage(image, 50, 0, 70, 70)
-
-  channel.send('<img src="' + canvas.toDataURL() + '" />')
-})
-});
-
-
-
 
 client.login(process.env.TOKEN);
