b260f84ebd6eda6c5bd92cddf1e7075f9c1b1537
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-04-28 14:38:45 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -195,7 +195,7 @@ if(args[0] == "aim"){
 function functionaimstats() {
 	sheets.spreadsheets.values.get({
     spreadsheetId: '1NrS1Uw3cg_UkYul5bYHiYAjKeopBU_aYXh2NRuLIXGw',
-    range: "ControlPanel!D1:F",
+    range: "ControlPanel!E1:E",
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
