2896a2c276c855314b93d3fb1dda483edd809d7e
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-05-02 19:06:01 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -190,7 +190,7 @@ message.channel.send("@everyone Free For All")
 }
 }//IF USER IS IN WAR CHANNELS
 
-if(command === 'counter') { //   bot-spamTB                                 bot-spamTESTserver                              TB1-WAR-CHAT                                   TB2-WAR-CHAT                                    TB3-WAR-CHAT                                 General-TESTSERVER                       EMOJITESTSERVER-testing room
+if(command === 'counter' || command === 'c') { //   bot-spamTB                                 bot-spamTESTserver                              TB1-WAR-CHAT                                   TB2-WAR-CHAT                                    TB3-WAR-CHAT                                 General-TESTSERVER                       EMOJITESTSERVER-testing room
 if (message.channel.id === '673261006918516741' || message.channel.id === '666305824813219870' || message.channel.id === '560885677475102740' || message.channel.id === '562757850477101063' || message.channel.id === '643140704624967694' || message.channel.id === '661221254958940220' || message.channel.id === '699707863148265512') {
 if(args[0] == "aim" && args[1] === "stats"){
 //stats
