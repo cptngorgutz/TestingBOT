379ec0862317ab0e3535493f486846571dbf7bd4
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-05-13 16:15:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -120,17 +120,19 @@ let page = 1;
 		if (page === pages.length) return;
 		message.channel.send("**Please type a page number** \nPage 1 - General Commands\nPage 2 - General commands 2\nPage 3 - War Commands\nPage 4- PVP Commands\n\nOr type `cancel` to cancel");
         const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 10000 });
-        console.log(collector)
+        //console.log(collector)
         collector.on('collect', message => {
             if (message.content == "1") {
-            message.channel.send("You are on page 1");
+            message.channel.send("You are already on page 1");
             } else if (message.content == "2") {
+			message.channel.bulkDelete(2)
             page++;
             embed.setDescription(pages[page-1]);
             embed.setFooter(`Page ${page} of ${pages.length} - Use reaction below to navigate to the previous/next pages`);
             msg.edit(embed)
 			r.remove(r.users.filter(u => !u.bot).first());
             } else if (message.content == "3") {
+			message.channel.bulkDelete(2)
             page++;
 			page++;
             embed.setDescription(pages[page-1]);
@@ -138,6 +140,7 @@ let page = 1;
             msg.edit(embed)
 			r.remove(r.users.filter(u => !u.bot).first());
             } else if (message.content == "4") {
+			message.channel.bulkDelete(2)
             page++;
 			page++;
 			page++;
