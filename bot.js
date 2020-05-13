ad9fa18c213844a1e8386c71c019c38dc0404f48
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-05-13 14:55:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -119,6 +119,12 @@ let page = 1;
 		menu.on('collect', r => {
             if (page === pages.length) return;
 		message.channel.send("**Please type a page number** \nPage 1 - General Commands\nPage 2 - General commands 2\nPage 3 - War Commands\nPage 4- PVP Commands")
+		if message.content.startsWith("1"){
+		page-----;
+		} else if message.content.startsWith("4"){
+		page+++++;
+		}
+		
 			r.remove(r.users.filter(u => !u.bot).first());
         })
 		
