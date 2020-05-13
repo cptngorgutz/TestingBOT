fa7c27509118e431e1d1d3d9e11a98e8d9330886
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-05-13 15:25:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -117,13 +117,22 @@ let page = 1;
 		})
 		
 		menu.on('collect', r => {
-            if (page === pages.length) return;
 		message.channel.send("**Please type a page number** \nPage 1 - General Commands\nPage 2 - General commands 2\nPage 3 - War Commands\nPage 4- PVP Commands")
+		if (page === 1) {
 		if (message.content.startsWith("1")){
-		page0;
-		} else if(message.content.startsWith("4")){
-		page4;
-		}
+			//do nothing
+		} else if (message.content.startsWith("2")){
+		page++;
+		} else if (message.content.startsWith("3")){
+		page++;
+		page++;
+		page++;
+		} else if (message.content.startsWith("4")){
+		page++;
+		page++;
+		page++;
+		page++;
+		}}
 		
 			r.remove(r.users.filter(u => !u.bot).first());
         })
