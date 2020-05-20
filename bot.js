cd593d215f95d18b5a131ff577f84a4732e5c6e2
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-05-20 21:29:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,21 +82,17 @@ const command = args.shift().toLowerCase();
 
 if(command === 'hide') {
 if(args[0] === "alpha"){
-function closeDownChannel(message) {
-	let guild = client.guilds.get("617707484626288670");
-	let channel = client.channels.get('712643585408761938');
-    const TB2Role = message.guild.roles.find(r => r.name === 'TB2');
+let guild = client.guilds.get("617707484626288670");
+let channel = client.channels.get('712643585408761938');
+const TB2Role = message.guild.roles.find(r => r.name === 'TB2');
 
-    // overwrites 'SEND_MESSAGES' role, only on this specific channel
-	channel.overwritePermissions([
-	{
-     id: TB2Role.id,
-     deny: ['VIEW_CHANNEL'],
-	},
-	], 'Lockdown');
-
-}
-closeDownChannel();
+// overwrites 'SEND_MESSAGES' role, only on this specific channel
+channel.overwritePermissions([
+{
+id: TB2Role.id,
+deny: ['VIEW_CHANNEL'],
+},
+], 'Lockdown');
 
 }
 
