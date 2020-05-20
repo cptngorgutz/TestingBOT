b4edd588daf5e8097d0496f0885ae9ccbcb73c5f
cptngorgutz <52824119+cptngorgutz@users.noreply.github.com>
2020-05-20 23:49:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,10 +82,9 @@ const command = args.shift().toLowerCase();
 
 if(command === 'hide') {
 if(args[0] === "alpha"){
-const TB2 = message.guild.roles.find(r => r.name === 'TB2');
 const channel1 = message.guild.channels.find(c => c.name === 'alpha-raid');
 
-channel1.overwritePermissions(message.guild.roles.find('name', '@TB2'), { // Disallow Everyone to see, join, invite, or speak
+channel1.overwritePermissions(message.guild.roles.find(r => r.name === 'TB2'), { // Disallow Everyone to see, join, invite, or speak
 'VIEW_CHANNEL': false,
 });
 message.channel.send('Alpha hidden from TB2.');
